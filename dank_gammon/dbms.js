5d31c569c0c80c81bee4260d6f76ca2174811ab8
Kotys Two <dantku@outlook.com>
2021-01-13 21:09:10 +0000

    Getting playlist data, need to work out what I want to do with it now

--- dank_gammon/dbms.js
+++ dank_gammon/dbms.js
@@ -4,7 +4,7 @@ const Datastore = require('nedb');
 
 
 options = {
-	filename : './db/collaboration_collection',
+	filename : '..rm/db/playlist_collection',
 	timestampData : true
 }
 db = new Datastore(options);
@@ -24,37 +24,52 @@ const instance = got.extend({
 });
 
 async function user_in (access_token, user)
+{
+	db.loadDatabase ((err) => {console.log(err); return;})
+	
+	const context = {
+		Authorization: 'Bearer ' + access_token 
+	};
+	let collaborative_playlists = [];
+	
+	const response = await instance('https://api.spotify.com/v1/me/playlists?limit=50&offset=0', {context}).json();		
+	_.map(response.items, (o) => { 
+		if (o.collaborative== true) { collaborative_playlists.push({ name : o.name, id : o.id })}
+		return
+	});
+	
+	total = response.total - 50;
+	offset = 50;
+	
+	while (total > 0)
 	{
-		db.loadDatabase ((err) => {console.log(err); return;})
-		
-		const context = {
-			Authorization: 'Bearer ' + access_token 
-		};
-		let collaborative_playlists = [];
-		
-		const response = await instance('https://api.spotify.com/v1/me/playlists?limit=50&offset=0', {context}).json();		
+		const response = await instance(`https://api.spotify.com/v1/me/playlists?limit=50&offset=${offset}`, {context}).json();		
 		_.map(response.items, (o) => { 
 			if (o.collaborative== true) { collaborative_playlists.push({ name : o.name, id : o.id })}
 			return
 		});
-		
-		total = response.total - 50;
-		offset = 50;
-		
-		while (total > 0)
-		{
-			const response = await instance(`https://api.spotify.com/v1/me/playlists?limit=50&offset=${offset}`, {context}).json();		
-			_.map(response.items, (o) => { 
-				if (o.collaborative== true) { collaborative_playlists.push({ name : o.name, id : o.id })}
-				return
-			});
-			total -= 50;
-			offset += 50;
-		}
-		
-		console.log(collaborative_playlists);
-
+		total -= 50;
+		offset += 50;
 	}
+	
+	// console.log(collaborative_playlists);
+	// store playlist data
+	
+	//
+	console.log("Getting playlist data...");
+	await get_playlist_data(context, collaborative_playlists);
+
+}
+	
+async function get_playlist_data(context, collaborative_playlists)
+{
+	for (playlist of collaborative_playlists)
+	{
+		const response = await instance(`https://api.spotify.com/v1/playlists/${playlist.id}/tracks`, {context}).json();
+		console.log(response);		
+	}
+	
+}
 	
 	
 module.exports.user_in = user_in;
