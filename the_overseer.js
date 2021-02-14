3df546c3bcbf72311c453c9a90defbc1ba14f4ff
Daniel Carey <danjcarey@outlook.com>
2021-02-14 13:53:50 +0000

    made db find user function asynchonous, and only called from db man module

--- the_overseer.js
+++ the_overseer.js
@@ -21,11 +21,9 @@ function oversee ()
 
 async function check_expiration ()
 {
-    // db.loadDatabase ((err) => {if(err){console.log(err)}; return;});
     let current_unix_time = Math.floor(new Date() / 1000);
     let date_object = new Date(current_unix_time * 1000);
 	let date_time = date_object.toLocaleString(); 
-    // console.log(current_unix_time);
 
     // Will update the ACCESS TOKEN when the current time is within 1.5x the check REFRESH TIME
     // (worst case being two refresh at 1.5*interval and 0.5 interval. However preferable to loss of access token)
@@ -39,7 +37,6 @@ async function check_expiration ()
                 console.log(date_time + ": " + user.name + " is expired:(");
                 dbman.update_user(user);
             }
-            // else if ( user.expire_time <= current_unix_time - 1.5*expiration_interval_secs )
             else if ( current_unix_time >= user.expire_time - 1.5*expiration_interval_secs  )
             {
                 console.log(date_time + ": " + user.name + " refreshed!");
@@ -50,38 +47,10 @@ async function check_expiration ()
     } catch (error) {
         console.log(err);
     }
-    
-    // db.find({ }, (err, docs) => {
-    //     if(!err)
-    //     {
-    //         // console.log(current_unix_time);
-    //         for (user of docs)
-    //         {
-    //             if ( current_unix_time >= user.expire_time  )
-    //             {
-    //                 user.is_expired = true;
-    //                 console.log(user.expire_time+ " : " + date_time + ": " + user.name + " is expired:(");
-    //                 userman.update_user(user);
-    //             }
-    //             // else if ( user.expire_time <= current_unix_time - 1.5*expiration_interval_secs )
-    //             else if ( current_unix_time >= user.expire_time - 1.5*expiration_interval_secs  )
-    //             {
-    //                 console.log(date_time + ": " + user.name + " refreshed!");
-    //                 spotify.refresh(user); 
-    //             }                
-    //         }
-    //     }
-    //     else
-    //     {
-    //         console.log("err");
-    //     }
-
-    // });
 }
 
 async function get_user_data()
 {
-    // let current_unix_time = Math.floor(new Date() / 1000);
     try {
         const users = await dbman.find_users({"is_expired":false});
         for (user of users)
