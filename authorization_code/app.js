5f13b1de91c1a5f328adab9a1f28d5936b0e1631
Daniel Carey <danjcarey@outlook.com>
2021-02-14 14:31:13 +0000

    retry push to heroku

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -36,6 +36,7 @@ for (const name of Object.keys(nets)) {
     }
 }
 
+// why wont it update this?
 const ip_address = results.en0      || results.enp1s0 || results.wlp3s0 || 'localhost';
 const url = "https://dankgammon.herokuapp.com"
 
